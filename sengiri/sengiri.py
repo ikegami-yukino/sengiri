75a91c50cf72452408ec3f274d0a3fe26f1d03fa
Yukino Ikegami <yknikgm@gmail.com>
2019-10-10 03:45:34 +0900

    Support full-width parenthesis

--- sengiri/sengiri.py
+++ sengiri/sengiri.py
@@ -3,11 +3,11 @@ import re
 import emoji
 import MeCab
 
-EMOJIS = ''.join(emoji.UNICODE_EMOJI)
-re_emoji = re.compile('([%s]{3,})(?!\))' % EMOJIS)
-DELIMITERS = {'。', '．', '…', '・・・', '...', '！', '!', '？', '?'}
-re_parenthesis = re.compile('(\([^！？\!\?。．・…%s]*[！？\!\?。．・…%s]+\))' % (EMOJIS, EMOJIS))
-re_delimiter = re.compile('\t([…。．！？\!\?―\t]+)(?!\))')
+EMOJIS = ''.join(emoji.UNICODE_EMOJI.keys())
+EMOJI_SET = set(EMOJIS)
+re_emoji = re.compile('\t([%s\t]{3,})(?![\)）])' % (EMOJIS))
+re_parenthesis = re.compile('([\(（][^！？\!\?。．・…]*[！？\!\?。．・…]+[\)）])')
+re_delimiter = re.compile('\t([…。．！？\!\?―\t\]]+)(?!\))')
 
 
 def tokenize(doc, mecab_args=''):
@@ -25,17 +25,15 @@ def tokenize(doc, mecab_args=''):
     list
         Sentences.
     """
-    doc = re_emoji.sub(lambda x: x.group(1)+'\n', doc)
     doc = re_parenthesis.sub(lambda x: '\n'+x.group(1)+'\n', doc)
 
     result = []
     for line in doc.splitlines():
-        if all(c not in line for c in DELIMITERS):
-            result.append(line)
-            continue
         tagger = MeCab.Tagger('-F %m\\t --eos-format=\n ' + mecab_args)
-        segmented_words = tagger.parse(line).rstrip()
+        segmented_words = tagger.parse(line)
         segmented_words = re_delimiter.sub(lambda x: x.group(1)+'\n', segmented_words)
+        if any(word in EMOJI_SET for word in segmented_words.split('\t')):
+            segmented_words = re_emoji.sub(lambda x: x.group(1)+'\n', segmented_words)
         segmented_words = segmented_words.replace('\t', '')
         for l in segmented_words.splitlines():
             result.append(l)
