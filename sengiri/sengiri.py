de4c7762f61aaf66d3d469052702eaa66d8dc638
Yukino Ikegami <yknikgm@gmail.com>
2019-10-11 02:39:56 +0900

    Use MeCab instead of regexp

--- sengiri/sengiri.py
+++ sengiri/sengiri.py
@@ -2,8 +2,42 @@ import re
 
 import MeCab
 
-re_delimiter = re.compile('\t([…。．！？\!\?―\t\]]+)(?!\))')
+DELIMITERS = set({'。', '．', '…', '・・・', '...', '！',
+                  '!', '？', '?', '！？', '？！', '!?', '?!'})
 re_parenthesis = re.compile('([%s])([\(（][^\)）]{10,}[\)）])' % ''.join(DELIMITERS))
+
+
+def _analyze_by_mecab(line, mecab_args):
+    tagger = MeCab.Tagger(mecab_args)
+    result = []
+    has_delimiter = False
+    for line in tagger.parse(line).splitlines():
+        line = line.rstrip()
+        if line == 'EOS':
+            break
+
+        (surface, features) = line.split('\t')
+
+        if (features.startswith('記号,一般,')
+            or any(surface == d for d in DELIMITERS)
+                or all(c in DELIMITERS for c in surface)):
+            has_delimiter = True
+        elif (result and result[-1][-1] not in ('http://', 'https://')
+                and surface in ('w', 'www')):
+            has_delimiter = True
+        elif has_delimiter is True and surface == '.' and result[-1][-1] in ('w', 'www'):
+            has_delimiter = False
+        elif has_delimiter is True:
+            has_delimiter = False
+            result[-1] = ''.join(result[-1])
+            result.append([])
+
+        if not result:
+            result.append([])
+        result[-1].append(surface)
+
+    result[-1] = ''.join(result[-1])
+    return result
 
 
 def tokenize(doc, mecab_args=''):
@@ -24,11 +58,6 @@ def tokenize(doc, mecab_args=''):
     doc = re_parenthesis.sub(lambda m: m.group(1) + '\n' + m.group(2) + '\n', doc)
 
     result = []
-    for line in doc.splitlines():
-        tagger = MeCab.Tagger('-F %m\\t --eos-format=\n ' + mecab_args)
-        segmented_words = tagger.parse(line)
-        segmented_words = re_delimiter.sub(lambda x: x.group(1)+'\n', segmented_words)
-        segmented_words = segmented_words.replace('\t', '')
-        for l in segmented_words.splitlines():
-            result.append(l)
+    for line in filter(bool, doc.splitlines()):
+        result += _analyze_by_mecab(line, mecab_args)
     return result
--- test_sengiri.py
+++ test_sengiri.py
@@ -2,11 +2,21 @@ from nose.tools import assert_equal
 import sengiri
 
 def test_tokenize():
-    actual = sengiri.tokenize('これは！(すばらしい！)感動……。')
-    assert_equal(actual, ['これは！', '(すばらしい！)', '感動……。'])
     actual = sengiri.tokenize('うーん🤔🤔🤔どうしよう')
     assert_equal(actual, ['うーん🤔🤔🤔', 'どうしよう'])
     actual = sengiri.tokenize('モー娘。のコンサートに行った。')
     assert_equal(actual, ['モー娘。のコンサートに行った。'])
     actual = sengiri.tokenize('楽しかったし嬉しかった。すごく充実した!')
     assert_equal(actual, ['楽しかったし嬉しかった。', 'すごく充実した!'])
+    actual = sengiri.tokenize('ありがとう＾＾ 助かります。')
+    assert_equal(actual, ['ありがとう＾＾', '助かります。'])
+    actual = sengiri.tokenize('子供が大変なことになった。'
+                              '（後で聞いたのだが、脅されたらしい）'
+                              '（脅迫はやめてほしいと言っているのに）')
+    assert_equal(actual, ['子供が大変なことになった。',
+                          '（後で聞いたのだが、脅されたらしい）',
+                          '（脅迫はやめてほしいと言っているのに）'])
+    actual = sengiri.tokenize('楽しかったw また遊ぼwww')
+    assert_equal(actual, ['楽しかったw', 'また遊ぼwww'])
+    actual = sengiri.tokenize('http://www.inpaku.go.jp/')
+    assert_equal(actual, ['http://www.inpaku.go.jp/'])
